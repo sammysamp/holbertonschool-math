5511b7b0144be4b0929c0d0384485b099fdaac7c
sammysamp <sammysamp@gmail.com>
2021-07-29 14:00:18 -0700

    Task 0: Display complex number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,16 +8,16 @@
 void display_complex_number(complex c)
 {
 	if (c.re != 0)
-		printf("%.6g ", c.re);
+		printf("%.16g ", c.re);
 	if (c.im < 0)
-		printf("- %.6gi\n", (-1) * c.im);
+		printf("- %.16gi\n", (-1) * c.im);
 	else
 		if (c.im == 0)
 			printf("\n");
 		else
 			if (c.re != 0)
-				printf("+ %.6gi\n", c.im);
+				printf("+ %.16gi\n", c.im);
 			else
-				printf("%.6gi\n", c.im);
+				printf("%.16gi\n", c.im);
 }
 
