b45e08e9bc9dff4beaf1f6223e6f4d7ffe0920ae
sammysamp <sammysamp@gmail.com>
2021-07-29 18:44:53 -0700

    Task 0: Display complex number

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <float.h>
+#include <limits.h>
 /**
  * display_complex_number - display complex number
  * @c: complex number to display
@@ -7,20 +9,26 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re != 0)
-		printf("%f ", c.re);
-	if (c.im < 0)
+	if (c.re < DBL_MAX && c.im < DBL_MAX)
+	{
 		if (c.re != 0)
-			printf("- %gi\n", (-1) * c.im);
-		else
-			printf("%gi\n", c.im);
-	else
-		if (c.im == 0)
-			printf("0\n");
-		else
+			printf("%g ", c.re);
+		if (c.im < 0)
 			if (c.re != 0)
-				printf("+ %gi\n", c.im);
+				printf("- %gi\n", (-1) * c.im);
 			else
 				printf("%gi\n", c.im);
+		else
+			if (c.im == 0)
+				if (c.re != 0)
+					printf("\n");
+				else
+					printf("0\n");
+			else
+				if (c.re != 0)
+					printf("+ %gi\n", c.im);
+				else
+					printf("%gi\n", c.im);
+	}
 }
 
